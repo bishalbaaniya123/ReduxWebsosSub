9cd41d419dda02bc2166ca8b1b2ab1d5c9e8a25a
Bishal Baaniya <071bct513@pcampus.edu.np>
2018-04-26 17:38:59 +0545

    first push

---
+++ client/components/Main.js
@@ -0,0 +1,17 @@
+import React, {createClass} from 'react';
+import {Link} from 'react-router';
+
+const Main = React.createClass({
+    render() {
+        return (
+            <div>
+                <h1>
+                    <Link to="/">Bishal</Link>
+                </h1>
+                {React.cloneElement(this.props.children, this.props)}
+            </div>
+        )
+    }
+});
+
+export default Main;
---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+    render() {
+        return (
+            <div className="single-photo">
+                I am the photogrid
+            </div>
+        );
+    }
+});
+
+export default PhotoGrid;
